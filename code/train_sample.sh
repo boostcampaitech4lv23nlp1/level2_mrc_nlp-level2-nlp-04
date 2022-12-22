80770704acbca89acc0ab94a2fecb4a890699005
54data <ehdvkf02@naver.com>
2022-12-22 03:59:36 +0000

    Feat : Add sample shell script

---
+++ code/train_sample.sh
@@ -0,0 +1,14 @@
+python train.py \
+--output_dir ./models/train_dataset \
+--do_train \
+--do_eval \
+--overwrite_output_dir True \
+--model_name_or_path "klue/bert-base" \
+--per_device_train_batch_size 8 \
+--per_device_eval_batch_size 8 \
+--num_train_epochs 3 \
+--weight_decay 0.01 \
+--warmup_ratio 0.1 \
+--learning_rate 3e-5 \
+--project_name "test" \
+--entity_name "ehdvkf02"
