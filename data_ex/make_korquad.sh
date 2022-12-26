a4f5460577b4c3ba7a80ed606868082e7fab633f
YeRyeongLee <yyi06011@khu.ac.kr>
2022-12-26 05:50:31 +0000

    Feat: get korquad and change schema

---
+++ data_ex/make_korquad.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/korquad/korquad.github.io/master/dataset/KorQuAD_v1.0_train.json -O KorQuAD_v1.0_train.json
+wget https://raw.githubusercontent.com/korquad/korquad.github.io/master/dataset/KorQuAD_v1.0_dev.json -O KorQuAD_v1.0_dev.json
+
+python korquad.py
