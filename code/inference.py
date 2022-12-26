c79eb8d9f2c16e3055174524496689e53784a95a
JunHyung <pjh120610@naver.com>
2022-12-26 15:36:57 +0000

    Refacor: run_retrieval split

--- code/inference.py
+++ code/inference.py
@@ -11,15 +11,7 @@ from typing import Callable, Dict, List, NoReturn, Tuple
 
 import numpy as np
 from arguments import DataTrainingArguments, ModelArguments
-from datasets import (
-    Dataset,
-    DatasetDict,
-    Features,
-    Sequence,
-    Value,
-    load_from_disk,
-    load_metric,
-)
+from datasets import load_from_disk
 from retrievals import *
 from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
@@ -34,6 +26,7 @@ from transformers import (
 )
 from utils_qa import check_no_error, postprocess_qa_predictions
 from run_mrc import run_mrc
+from run_retrieval import run_sparseretrieval
 
 logger = logging.getLogger(__name__)
 
@@ -87,67 +80,13 @@ def main():
 
     # True일 경우 : run passage retrieval
     if data_args.eval_retrieval:
-        datasets = run_retrieval(
+        datasets = run_sparseretrieval(
             tokenizer.tokenize, datasets, training_args, data_args,
         )
 
     # eval or predict mrc model
     if training_args.do_eval or training_args.do_predict:
         run_mrc(data_args, training_args, model_args, datasets, tokenizer, model, "inference", logger)
-
-
-def run_retrieval(
-    tokenize_fn: Callable[[str], List[str]],
-    datasets: DatasetDict,
-    training_args: TrainingArguments,
-    data_args: DataTrainingArguments,
-    data_path: str = "../data",
-    context_path: str = "wikipedia_documents.json",
-) -> DatasetDict:
-
-    # Query에 맞는 Passage들을 Retrieval 합니다.
-    retriever = getattr(sys.modules[__name__], data_args.retrieval_type)(
-        tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
-    )
-    retriever.get_embedding()
-
-    if data_args.use_faiss:
-        retriever.build_faiss(num_clusters=data_args.num_clusters)
-        df = retriever.retrieve_faiss(
-            datasets["validation"], topk=data_args.top_k_retrieval
-        )
-    else:
-        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
-
-    # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
-    if training_args.do_predict:
-        f = Features(
-            {
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-
-    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
-    elif training_args.do_eval:
-        f = Features(
-            {
-                "answers": Sequence(
-                    feature={
-                        "text": Value(dtype="string", id=None),
-                        "answer_start": Value(dtype="int32", id=None),
-                    },
-                    length=-1,
-                    id=None,
-                ),
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    return datasets
 
 
 if __name__ == "__main__":
