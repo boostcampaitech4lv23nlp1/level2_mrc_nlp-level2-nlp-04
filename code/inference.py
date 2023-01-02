0383665db5fd7045d522f857fabcf408ac723b89
54data <ehdvkf02@naver.com>
2023-01-02 02:46:25 +0000

    Feat: Add run_elasticsearch_retrieval function

--- code/inference.py
+++ code/inference.py
@@ -12,7 +12,6 @@ import numpy as np
 from arguments import DataTrainingArguments, ModelArguments, RetrievalArguments
 from datasets import load_from_disk
 from retrievals import *
-from elasticsearch_retrieval import ElasticSearchRetrieval
 from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
@@ -26,7 +25,7 @@ from transformers import (
 )
 
 from run_mrc import run_mrc
-from run_retrieval import run_sparseretrieval, run_denseretrieval
+from run_retrieval import run_sparseretrieval, run_denseretrieval, run_elasticsearch_retrieval
 
 logger = logging.getLogger(__name__)
 
@@ -92,6 +91,10 @@ def main():
             datasets = run_denseretrieval(
                 datasets, training_args, data_args, retrieval_args
             )
+        elif retrieval_args.retrieval_type == "elasticsearch":
+            datasets = run_elasticsearch_retrieval(
+                datasets, training_args, data_args, retrieval_args
+            )            
         else:
             print("retrieval_type 확인")
             exit(1)
@@ -101,63 +104,5 @@ def main():
         run_mrc(data_args, training_args, model_args, datasets, tokenizer, model, "inference", logger)
 
 
-<<<<<<< HEAD
-def run_retrieval(
-    tokenize_fn: Callable[[str], List[str]],
-    datasets: DatasetDict,
-    training_args: TrainingArguments,
-    data_args: DataTrainingArguments,
-    data_path: str = "../data",
-    context_path: str = "wikipedia_documents.json",
-) -> DatasetDict:
-
-    # Query에 맞는 Passage들을 Retrieval 합니다.
-    retriever = getattr(sys.modules[__name__], data_args.retrieval_type)(
-        tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
-    )
-    retriever.get_embedding()
-    #retriever = ElasticSearchRetrieval(index_name=data_args.index_name)
-
-    if data_args.use_faiss:
-        retriever.build_faiss(num_clusters=data_args.num_clusters)
-        df = retriever.retrieve_faiss(
-            datasets["validation"], topk=data_args.top_k_retrieval
-        )
-    else:
-        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
-
-    # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
-    if training_args.do_predict:
-        f = Features(
-            {
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-
-    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
-    elif training_args.do_eval:
-        f = Features(
-            {
-                "answers": Sequence(
-                    feature={
-                        "text": Value(dtype="string", id=None),
-                        "answer_start": Value(dtype="int32", id=None),
-                    },
-                    length=-1,
-                    id=None,
-                ),
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    return datasets
-
-
-=======
->>>>>>> dev
 if __name__ == "__main__":
     main()
