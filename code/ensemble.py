c01e46d8fca5f91b1710389f3ba6c646051cbd2a
root <wdg369@gmail.com>
2023-01-05 05:53:33 +0000

    Feature: weighted_voting

--- code/ensemble.py
+++ code/ensemble.py
@@ -2,8 +2,9 @@ import pandas as pd
 import json
 import os
 import argparse
+import natsort
 
-def summation_socres_of_same_strings(predictions, num_cands):
+def soft_voting(predictions, num_cands):
     predictions_df = predictions
     summation_df = {}
     
@@ -13,8 +14,6 @@ def summation_socres_of_same_strings(predictions, num_cands):
         
         for row in range(0, 20 * num_cands): 
             logit_dict = predictions_df.iloc[:, col][row]
-            # print("#####", logit_dict)
-            # print("#####", type(logit_dict))
 
             text = logit_dict['text']
             probability = logit_dict['probability']
@@ -28,21 +27,56 @@ def summation_socres_of_same_strings(predictions, num_cands):
         summation_df[predictions_df.columns[col]] = sorted(summation_dict.items(), reverse=True, key=lambda item: item[1])[0][0]
     return summation_df
 
+
+
+def weighted_voting(predictions, weights, num_cands):
+    predictions_df = predictions
+    summation_df = {}
+    
+    for col in range(len(predictions_df.columns)):
+        summation_dict = {}
+        summation_df[predictions_df.columns[col]] = None
+        
+        for row in range(0, 20 * num_cands): 
+            logit_dict = predictions_df.iloc[:, col][row]
+
+            text = logit_dict['text']
+            if row % 20 == 0:
+                try: 
+                    weight = int(weights.pop(0))
+                    # print(weight, row)
+                except:
+                    pass
+            probability = logit_dict['probability'] * weight
+        
+            try:
+                summation_dict[text] += probability
+            except KeyError:
+                summation_dict[text] = probability
+        
+        # 점수가 높은 순으로 정렬한 후 가장 점수가 높은 answer만 가져옵니다.
+        summation_df[predictions_df.columns[col]] = sorted(summation_dict.items(), reverse=True, key=lambda item: item[1])[0][0]
+    return summation_df
+
+
 def main():
     # ensemble candidates
     cand_path = args.cand_dir
     num_cands = len(os.listdir(cand_path))
-
     cands = pd.DataFrame()
-    for cand in os.listdir(cand_path):
+    for cand in natsort.natsorted(os.listdir(cand_path)):
+        print("cand: ", cand)
         df = pd.read_json(os.path.join(cand_path, cand))
         if len(cands) > 1:
             cands = pd.concat([cands, df])
         else:
             cands = df
     cands.reset_index(inplace=True, drop=True)
-    print(cands)
-    result = summation_socres_of_same_strings(cands, num_cands)
+
+    if weighted_voting:
+        result = weighted_voting(cands, args.weights.split(), num_cands)
+    else:
+        result = soft_voting(cands, num_cands)
 
     with open(os.path.join(args.result_dir, "ensemble_result.json"), "w", encoding="utf-8") as f:
         f.write(
@@ -65,7 +99,18 @@ if __name__ == '__main__':
         type=str,
         help="dir which the results is in"
     )
-
+    parser.add_argument(
+        "--weighted_voting",
+        default=False,
+        type=bool,
+        help="Whether do weighted voting or not"
+    )
+    parser.add_argument(
+        "--weights",
+        default=None,
+        type=str,
+        help="Weights"
+    )
     args = parser.parse_args()
 
     main()
